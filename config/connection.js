64936d5ef8de71ebe2939ce418e70cae6aaffc1f
James Plasencia <jamesplasencia@gmail.com>
2022-08-04 21:32:53 -0700

    stuff

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+
 const Sequelize = require("sequelize");
 require("dotenv").config();
-=======
-const Sequelize = require('sequelize');
-require('dotenv').config();
->>>>>>> 08c7813125295ba87e216a5e83f2e118daf771eb
 
 let sequelize;
 
@@ -35,20 +31,3 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports = sequelize;
-=======
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
-
-module.exports = sequelize;
->>>>>>> 08c7813125295ba87e216a5e83f2e118daf771eb
