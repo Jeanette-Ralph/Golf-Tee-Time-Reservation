c5def0ea65f4b0ddf9704481a6d15d59274ebd35
Jeanette Ralph <jean.ralph@hotmail.com>
2022-08-05 18:32:49 -0700

    changed user.js to User.js, worked on user seeds, did sign in nav bar partial, did js for login and logout

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const userLogout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+const logoutButton = document.querySelector('#logout').addEventListener('click', userLogout);
