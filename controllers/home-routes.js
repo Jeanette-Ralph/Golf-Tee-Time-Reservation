43c8e2af63f4197ef9adb81c4ba028a41a908584
James Plasencia <jamesplasencia@gmail.com>
2022-08-11 16:03:56 -0700

    jp push

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,18 +4,11 @@ const { Teetimes, User } = require("../models");
 router.get("/", async (req, res) => {
   try {
     const dbTeetimesData = await Teetimes.findAll({});
-
-    const dbUserData = await User.findAll({});
-
     const teetimes = dbTeetimesData.map((teetimes) =>
       teetimes.get({ plain: true })
     );
-
-    const user = dbUserData.map((user) => user.get({ plain: true }));
-
     res.render("home", {
       teetimes,
-      user,
     });
   } catch (err) {
     res.status(500).json(err);
