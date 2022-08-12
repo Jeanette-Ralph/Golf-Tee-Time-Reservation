c2bd1736b675fb0b4ffa6e9a9fd6e5bf2d150e7b
Jeanette Ralph <jean.ralph@hotmail.com>
2022-08-11 17:46:09 -0700

    done

--- controllers/api/bookTeeTimeRoute.js
+++ controllers/api/bookTeeTimeRoute.js
@@ -2,11 +2,8 @@ const router = require("express").Router();
 const { Teetimes, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 const nodemailer = require("nodemailer");
-// need an util-auth if we go old ways with auth
-// const withAuth = require('../../utils/auth');
 
-// go to book rout after auth?
-router.get("/", withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const timeData = await Teetimes.findAll();
     const time = timeData.map((times) => times.get({ plain: true }));
@@ -14,85 +11,62 @@ router.get("/", withAuth, async (req, res) => {
       time,
       loggedIn: req.session.loggedIn,
       user_id: req.session.user_id,
+      loggedIn: true,
     });
-    console.log("---------------------logged in------------------------------");
-    console.log(req.session.loggedIn);
-    console.log("---------------------user id------------------------------");
-    console.log(req.session.user_id);
-    console.log("----------------------session-----------------------------");
-    console.log(req.session);
-    // res.status(200).json(timeData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 // updates cards with individual times with a user id to associate with
-router.put("/:id", async (req, res) => {
+router.put("/:id", withAuth, async (req, res) => {
   try {
-    console.log("about to req.body");
-    console.log(req.body);
-    // undefined value
     const objUp = {
       ...req.body,
       user_id: req.session.user_id,
-      // hard code user id
-      // user_id: 4,
+      loggedIn: true,
     };
-    console.log(objUp);
 
     const timeData = await Teetimes.update(objUp, {
       where: { id: req.params.id },
     });
     if (!timeData) {
-      console.log("great");
       res.status(400).json({ message: "can not update an invalid tee time" });
       return;
     }
     res.status(200).json(timeData);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
-// logged in users booked teetimes
-// post works in insomnia-now to link to the button
 router.post("/send", async (req, res) => {
   try {
+    const eUse = await User.findOne({
+      where: { email: req.body.email },
+    });
+    const uEmail = eUse.get({ plain: true });
+
+    if (!uEmail) {
+      res.status(400).json({ message: "user email error" });
+      return;
+    }
     const output = `
-<p>You have booked you tee time!</p>
-<h3>Golf N' Stuff Links</h3>
-<ul>  
-    </ul>
-    <h3>Message</h3>
-    <p>Congrats on booking your tee time. Please arrive 15 min before your time.
-    The parking lot is located at 
-    742 Evergreen Terrace
-    Springfield</p>
+    <p>You have booked you tee time!</p>
+    <h3>Golf N' Stuff Links</h3>
+    <ul>
+        </ul>
+        <h3>Message</h3>
+        <p>Congrats on booking your tee time. Please arrive 15 min before your time.
+        The parking lot is located at
+        742 Evergreen Terrace
+        Springfield</p>
+      `;
 
-  `;
-    const userData = await User.findOne({
-      where: { email: req.body.name },
-    });
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      req.session.email = userData.email;
-
-      res.status(200).json(userData);
-    });
-    // const getEmail = {
-    //   // ...req.body,
-    //   // email: req.session.email,
-    //   // hard code user id
-    //   email: "jamesplasencia@gmail.com",
-    // };
-    // this is copy pasta from nodemailer
-    // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-      host: process.env.HOST, //'mail.YOURDOMAIN.com',
+      host: process.env.HOST,
       port: process.env.PORTEMAIL,
-      secure: true, // true for 465, false for other ports
+      secure: true,
+      pool: true, // This is the field you need to add
       auth: {
         user: process.env.USER,
         pass: process.env.PASSWORD,
@@ -103,23 +77,20 @@ router.post("/send", async (req, res) => {
     });
 
     let mailOptions = {
-      from: '"Golf N Stuff Links" <mvpcents@gmail.com>', // sender address
-      to: `${req.body.email}`, //  "jamesplasencia@gmail.com"list of receivers
-      // to: , // list of receivers
-      subject: "Congratulations on the tee time", // Subject line
-      text: "Ready to play?", // plain text body
-      html: output, // html body
+      from: '"Golf N Stuff Links" <mvpcents@gmail.com>',
+      to: uEmail.email,
+      subject: "Congratulations on the tee time",
+      text: "Ready to play?",
+      html: output,
+      amp: `amp-img src="./public/images/Lee_Carvallo_s_Putting_Challenge.jpg"`,
     };
 
-    // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         return console.log(error);
       }
       console.log("Message sent: %s", info.messageId);
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-      res.render("contact", { msg: "Email has been sent" });
     });
   } catch (err) {
     res.status(400).json(err);
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,38 +1,18 @@
-// to fix an error
 const router = require("express").Router();
 const { Teetimes, User } = require("../models");
-// for authenticating the user to view their profile
-// const withAuth = require("../utils/auth");
-// Render the homepage because we are not logged in yet.
-// Render all of the tetimes
+
 router.get("/", async (req, res) => {
   try {
     const dbTeetimesData = await Teetimes.findAll({});
-
-    const dbUserData = await User.findAll({});
-
     const teetimes = dbTeetimesData.map((teetimes) =>
       teetimes.get({ plain: true })
     );
-
-    const user = dbUserData.map((user) => user.get({ plain: true }));
-
     res.render("home", {
       teetimes,
-      user,
     });
-    console.log(user);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
-
-// router.get("/user", withAuth, async (req, res) => {
-//   res.render("user", {
-//     user_id: req.session.user_id,
-//     loggedIn: req.session.loggedIn,
-//   });
-// });
 
 module.exports = router;
